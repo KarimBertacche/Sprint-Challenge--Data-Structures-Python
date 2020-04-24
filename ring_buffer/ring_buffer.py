c85f1e56318ea7b06cbe3793d3f758db42b5809f
Karim B <mattiakarim@live.it>
2020-04-24 10:05:41 +0200

    Complete ring buffer challenge, all tests are passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -16,20 +16,45 @@ class RingBuffer:
             self.current = self.storage.head
         # else if storage has reached the capacity
         elif len(self.storage) == self.capacity:
-            # make the new least used item the next item in the list after the head
-            # by keeping track of it in the current
-            self.current = self.current.next
+            # grab the older item from the head
+            oldest_item = self.storage.head
             # remove the item from the storage head
             self.storage.remove_from_head()
-            # place the item to the tail of the queue
+            # place the newly added item to the tail of the queue
             self.storage.add_to_tail(item)
+            # oldest item is the current 
+            if oldest_item is self.current:
+                # then make the current the tail of the storage
+                self.current = self.storage.tail
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        # grab the starting node
+        initial_node = self.current
+        # append the value from the starting node to the list buffer
+        list_buffer_contents.append(initial_node.value)
+        # if the starting node as a next
+        if initial_node.next is not None:
+            # use that has the following node
+            next_node = initial_node.next
+        else:
+            # else use the head of the storage
+            next_node = self.storage.head
 
+        # while there are other node keep iterating
+        while next_node != initial_node:
+            # append the very next node value on each iteration to the buffer list
+            list_buffer_contents.append(next_node.value)
+            # check if there is a next otherwise make storage head it
+            if next_node.next is not None:
+                next_node = next_node.next
+            else:
+                next_node = self.storage.head
+        
+        # return list of buffer
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
