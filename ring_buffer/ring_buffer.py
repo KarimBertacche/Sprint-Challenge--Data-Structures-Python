afa4ddccb1559d931d752e156f9a7c43310fa755
Karim B <mattiakarim@live.it>
2020-04-24 09:14:08 +0200

    Added first pseudo code algo for the ring buffer challenge

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,6 +8,15 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
+        # check whether the current storage length is less than the capacity
+            # and add the least used item in the head as the current for future removal
+        # else if storage has reached the capacity
+            # make the new least used item the next item in the list after the head
+            # by keeping track of it in the current
+
+            # remove the item from the storage head
+
+        # place the item to the tail of the queue
         pass
 
     def get(self):
