ebe060525d447358c771235f6d363ecc7ae5231e
Karim B <mattiakarim@live.it>
2020-04-24 10:37:51 +0200

    Complete challenge to get unique name in under a tenth of a second

--- names/names.py
+++ names/names.py
@@ -13,10 +13,25 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# create a new dictionary
+names_count = {}
+
+# iterate over the second list of names 
+for name in names_2:
+    # save the count of names in the new dict
+    names_count[name] = 1
+
+# iterate of the other list of names
+for name in names_1:
+    # check if name is present in the list
+    if names_count.get(name) != None:
+        # if so append it to the list of names
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
